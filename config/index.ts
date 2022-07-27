d4cfbe5daf117ce2781601597fc35c7413fb160f
zh19950508 <zhaohuiv@yonyou.com>
2022-07-27 23:59:30 +0800

    1

---
+++ config/index.ts
@@ -0,0 +1,26 @@
+let Config = {
+  blogCode: "zxm_blog", // 唯一code
+  apiUrl: "", // api服务器地址
+  walineServerURL: "", // 评论服务器地址
+  env: "",
+};
+
+if (process.env.NODE_ENV === "development") {
+  // 开发环境配置
+  Config.apiUrl = "http://127.0.0.1:7001";
+  Config.walineServerURL = "http://127.0.0.1:8360";
+  Config.env = "development";
+} else if (process.env.NODE_ENV === "production") {
+  // 线上环境配置
+  Config.env = "production";
+  // 线上浏览器环境 不能使用本地地址 需要配置远程地址
+  if (process.browser) {
+    Config.apiUrl = "https://x.abczzz.cn";
+    Config.walineServerURL = "https://wl.abczzz.cn";
+  } else {
+    Config.apiUrl = "http://127.0.0.1:7001";
+    Config.walineServerURL = "http://127.0.0.1:8360";
+  }
+}
+
+export default Config;
